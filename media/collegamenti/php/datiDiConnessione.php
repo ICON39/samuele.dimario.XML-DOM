35e8d7111ebbf246c5358c66023c2674871d4f09
ICON39 <164856080+ICON39@users.noreply.github.com>
2024-12-21 14:49:33 +0100

    Update datiDiConnessione.php

--- media/collegamenti/php/datiDiConnessione.php
+++ media/collegamenti/php/datiDiConnessione.php
@@ -3,5 +3,5 @@
 $host = "localhost";
 $user = 'root';
 $password = 'homework2';  
-$db = 'samuele.dimario.XML-DOM';         //nome del DB
+$db = 'samuele_dimario_XML_DOM';         //nome del DB
 $table_name = 'utenti';
